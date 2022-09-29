af6a9d99a7e9a0ead42cda78a1f208c29fdee16b
Naureen <98129474+goldfishdolphin@users.noreply.github.com>
2022-09-29 11:16:06 +0100

    Revert "Comments by article"

--- src/Components/IndividualArticle.jsx
+++ src/Components/IndividualArticle.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getArticle, patchArticle } from "../Utils/api";
 import moment from "moment";
-import { Link } from "react-router-dom";
 
 const IndividualArticle = () => {
     const { article_id } = useParams();
@@ -18,7 +17,7 @@ const IndividualArticle = () => {
             setVotesInc((currVotes) => currVotes + 1);
             setHasVoted(true);
         } else {
-            setVotesMsg('⛔ You have already voted. Sorry you cannot vote more than once.');
+            setVotesMsg('You have already voted. Sorry you cannot vote more than once.');
 
         }
     };
@@ -41,8 +40,6 @@ const IndividualArticle = () => {
                 <li>Votes :{article.votes}</li>
                 <button onClick={() => handleVotesIncrement(article.article_id)}>{votesInc}<span aria-label={`votes for article ${article.title}`}> 👍</span></button>
                 <p id='vote_msg'>{votesMsg}</p>
-                <Link to={`/articles/${article.article_id}/comments`}>View Comments ✍️ </Link>
-
 
 
             </ul>

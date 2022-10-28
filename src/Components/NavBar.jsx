419a748e7ea58abe8bf06d9b04937a0034da7cd4
Naureen <naureen.imran123@yahoo.com>
2022-10-28 07:52:57 +0100

    restyle nav bar user

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -11,19 +11,8 @@ const NavBar = () => {
     return (
         <>
             <Navbar className="responsive-navbar-nav p-4" variant='light' collapseOnSelect expand="lg">
-                <Container> Welcome
-                    <Navbar.Brand href="/">
-                        <img src={loggedInUser.avatar_url}
-                            alt={`avatar of ${loggedInUser.name}`}
-                            width="50px"
-                            height="100%"
-                            className="d-inline-block align-top"
-                        />
-                        <Navbar.Text className="p-1">{' '}{loggedInUser.username}
+                <Container>
 
-                            <Button onClick={() => { value.setLoggedInUser(null); }}>Log out</Button>
-                        </Navbar.Text>
-                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="nav me-auto">
@@ -33,6 +22,18 @@ const NavBar = () => {
                             <Nav.Link className='nav-link' href="/users">Users</Nav.Link>
 
                         </Nav>
+                        <Navbar.Brand href="/">
+                            <img src={loggedInUser.avatar_url}
+                                alt={`avatar of ${loggedInUser.name}`}
+                                width="50px"
+                                height="100%"
+                                className="d-inline-block align-top"
+                            />
+                            <Navbar.Text className="p-1">{'Logged In: '}{loggedInUser.username}
+
+                                <Button className='m-2' onClick={() => { value.setLoggedInUser(null); }}>Log out</Button>
+                            </Navbar.Text>
+                        </Navbar.Brand>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
